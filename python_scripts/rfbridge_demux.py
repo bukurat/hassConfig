41544781ec0f3898fd3c234354d963f774f3fa74
John <johnbuxt@gmail.com>
2019-04-03 17:23:58 +1100

    Changed Binary sensors and RF switches

--- python_scripts/rfbridge_demux.py
+++ python_scripts/rfbridge_demux.py
@@ -1,11 +1,33 @@
 #'payload':['topic','state','retain']
 
 d = { 'D3276E':['Hall_Motion','ON','false'],
-      'BC3E0A':['Back_Door','OFF','true'],
-      'BC3E0E':['Back_Door','ON','true'],
+      'BC3E0A':['Back_Door','ON','true'], #Closed
+      'BC3E0E':['Back_Door','OFF','true'],
       '30D8A0':['sensor3','ON','false'],
-      '86CC0B':['Power C', 'ON', 'true'],
-      '86CC0A':['Power C', 'OFF', 'true']
+      '86CC0F':['Power-A', 'ON', 'true'],
+      '86CC0E':['Power_A', 'OFF', 'true'],
+      '86CC0D':['Power_B', 'ON', 'true'],
+      '86CC0C':['Power_B', 'OFF', 'true'],
+      '86CC0B':['Power_C', 'ON', 'true'],
+      '86CC0A':['Power_C', 'OFF', 'true'],
+      '0844E0':['Push_Button','OFF','false'],
+      '0844E1':['Push_Button','ON','false'],
+      '00000F':['NPow1','ON','true'],
+      '00000E':['NPow1','OFF','true'],
+      '000007':['NPow2', 'ON','true'],
+      '000006':['NPow2','OFF','true'],
+      '00000B':['NPow3','ON','true'],
+      '00000A':['NPow3','OFF','true'],
+      '000003':['NP0w2','ON','true'],
+      '000002':['NPow2','OFF','true'],
+      '00000D':['NPall','ON', 'true'],
+      '00000C':['NPall','OFF','true'],
+      '84262E':['MainBed','ON','true'], # Closed
+      '842627':['MainBed','OFF','true'], # Open
+      '8B460A':['Office','ON','true'],
+      '8B460E':['Office','OFF','true']
+
+
     }
 
 p = data.get('payload')
--- www/kiosk-config.js
+++ www/kiosk-config.js
@@ -1,3 +1,3 @@
 setTimeout(function() {
-    LovelaceFullyKiosk.bind("7835d343-2a198cc8", "light.dashboard_screen", "binary_sensor.dashboard_motion");
+    LovelaceFullyKiosk.bind("cc6ef07d-b7b99635", "light.dashboard_screen", "binary_sensor.dashboard_motion");
   }, 200);
